d20a555209a4a5c0a4038ce9fa4e729d4d7ef3de
Ulweyn <ulweyn@yahoo.com>
2021-12-24 17:43:55 +0200

    added model, factory and seeder for product images

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,16 +9,7 @@ class HomeController extends Controller
 {
     public function index(){
         $products = Product::where('id',5)->first();
-        /*foreach ($products as $product){
-            echo 'title: '.$product->title;
-            echo "<br>";
-            echo 'price: '.$product->price;
-            echo "<br>";
-            echo "------------------------";
-            echo "<br>";
 
-        }*/
-        dd($products);
-        //return view('home.index');
+        return view('home.index');
     }
 }
