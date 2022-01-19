5d6092df8d74001900a781c7d6a070cb4d0faa26
Ulweyn <ulweyn@yahoo.com>
2022-01-19 19:34:44 +0200

    added category view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,10 @@ class ProductController extends Controller
         ]);
     }
 
-    public function showCategory(){
-
+    public function showCategory($cat_alias){
+        $cat = Category::where('alias',$cat_alias)->first();
+        return view('categories.index',[
+            'cat' => $cat
+        ]);
     }
 }
